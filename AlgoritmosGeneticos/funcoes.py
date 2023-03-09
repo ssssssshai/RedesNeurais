08ac888096e684c8711042331f19caee98c81984
Your NameShaian <you@example.comshaian220071@ilum.cnpem.br>
2023-03-09 17:36:28 -0300

    Resolução do exercício A.02 e funções

--- AlgoritmosGeneticos/funcoes.py
+++ AlgoritmosGeneticos/funcoes.py
@@ -1,1 +1,9 @@
- 
+def funcao_objetivo_caixabinaria(individuo):
+    '''
+    Computa a função objetivo no problema das caixas binárias.
+    Argumentos:
+    individuo lista contendo os genes das caixas binárias.
+    Return:
+    Um valor representando a soma dos genes do indivíduo.
+    '''
+    return sum(individuo)
